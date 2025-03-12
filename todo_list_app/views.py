4b8bf1d9e26cf93af140eec8d6afa8fa091b5d47
Ilia Terentev <wenhun1@gmail.com>
2025-03-12 13:46:38 +0200

    add list tasks on home page change Index view class to ListView

--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from todo_list_app.models import Task
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "todo_list/index.html"
-
-    def get_context_data(self, **kwargs) -> dict:
-        context = super().get_context_data(**kwargs)
-        tasks = Task.objects.prefetch_related("tags").all()
-
-        context.update(
-            {
-                "tasks": tasks,
-            }
-        )
-
-        return context
+    model = Task
+    paginate_by = 5
