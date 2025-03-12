8cc178dd35465222088ef003563ebd58292f0e68
Ilia Terentev <wenhun1@gmail.com>
2025-03-12 12:30:35 +0200

    bring project to flake8 standards remove SECRET_KEY form repo

--- todo_list_app/apps.py
+++ todo_list_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TodoListAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'todo_list_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "todo_list_app"
