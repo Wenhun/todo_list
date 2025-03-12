4a9c9e31730a812b2d53feb5a0999a1162d78a45
Ilia Terentev <wenhun1@gmail.com>
2025-03-12 13:17:50 +0200

    add IndexView class in views.py

--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from todo_list_app.models import Task
+
+
+class IndexView(TemplateView):
+    template_name = "todo_list/index.html"
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        tasks = Task.objects.prefetch_related("tags").all()
+
+        context.update(
+            {
+                "tasks": tasks,
+            }
+        )
+
+        return context
